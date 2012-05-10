04dad04631465ca58e405f130792ad83e2f40050
Mark Ferrer <self@markferrer.com>
2012-05-10 15:33:57 -0700

    added necessary files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='mandrill',
+    version='0.1a',
+    description='Python wrapper for the MailChimp Mandrill API',
+    author='Mark Ferrer',
+    author_email='mark@markferrer.com',
+    url='http://github.com/markferrer/python-mandrill',
+    packages=['mandrill']
+)
