b887a647697410a9ad6a64071bb5935cd20cf7ef
Mark Ferrer <self@markferrer.com>
2012-05-16 11:12:43 -0700

    added empty exception classes

---
+++ mandrill/exceptions.py
@@ -0,0 +1,19 @@
+
+
+class ParamaterError(BaseException):
+    return
+
+
+class MissingParameterError(ParameterError):
+    return
+
+
+class InvalidParamaterError(ParameterError):
+    return
+
+
+class ApiKeyError(BaseException):
+    """
+    Check to see if you supplied the correct api key.
+    """
+    return
